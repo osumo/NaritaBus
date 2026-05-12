8698882fc7a8e0b0df532f3f0a194edb3413f1d7
MASUMURA YUKI <hba.masumura@gmail.com>
2026-05-12 22:22:56 +0900

    first commit

---
+++ src/main/java/com/example/demo/MybatisSample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisSample1Application.class, args);
+	}
+
+}
